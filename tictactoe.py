af991487c0f5c68fb321819e08f4a7e759e77879
kyawzinmyat <72294831+kyawzinmyat@users.noreply.github.com>
2022-08-25 03:28:14 +0630

    initial commit

---
+++ tictactoe.py
@@ -0,0 +1,167 @@
+"""
+Tic Tac Toe Player
+"""
+
+import copy
+
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    term = 0
+    for row in board:
+        term += row.count(EMPTY)
+    if term:
+        return X if term %2 != 0 else O
+    return "The game is over"
+
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    set_of_actions = []
+    for i in range(3):
+        for j in range(3):
+            if not board[i][j]:
+                set_of_actions.append((i, j))
+    return set_of_actions
+
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    new_state = copy.deepcopy(board)
+    if new_state[action[0]][action[1]]:
+        return Exception
+    new_state[action[0]][action[1]] = player(board)
+    return new_state
+
+
+def row_col_check(board):
+    for index_col, row in enumerate(board):
+        if row.count(X) == 3:
+            return X
+        elif row.count(O) == 3:
+            return O
+        result = ''
+        for index_row, col in enumerate(row):
+            try:
+                result +=  board[index_row][index_col]
+            except:
+                pass
+        if result == X*3:
+            return X
+        elif result == O*3:
+            return O
+    ## diagonal check
+    diagonal = ''
+    try:
+        diagonal = board[0][0] + board[1][1] + board[2][2]
+    except:
+        pass
+    if diagonal == 3 * X:
+        return X
+    elif diagonal == 3 * O:
+            return O
+    try:
+        diagonal = board[2][0] + board[1][1] + board[0][2]
+    except:
+        pass
+    if diagonal == 3 * X:
+        return X
+    elif diagonal == 3 * O:
+            return O
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    return row_col_check(board)
+
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    if winner(board):
+        return True
+
+    for row in board:
+        if EMPTY in row:
+            return False
+    return True
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    result = winner(board)
+    if result == X:
+        return 1
+    elif result == O:
+        return -1
+    return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if not terminal(board):
+        o_action = None
+        value = -math.inf
+        if player(board) == X:
+            for action in actions(board):
+                n_value = Min(result(board, action))
+                if n_value > value:
+                    value = n_value
+                    o_action = action
+        value = math.inf
+        if player(board) == O:
+            for action in actions(board):
+                n_value = Max(result(board, action))
+                if n_value < value:
+                    value = n_value
+                    o_action = action
+        return o_action
+
+
+def Max(board):
+    if not terminal(board):
+        value = -math.inf
+        for action in actions(board):
+            value = max(value, Min(result(board, action)))
+        return value
+    return utility(board)
+
+def Min(board):
+    if not terminal(board):
+        value = math.inf
+        for action in actions(board):
+            value = min(value, Max(result(board, action)))
+        return value
+    return utility(board)
+
+
